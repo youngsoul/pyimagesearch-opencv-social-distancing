62a36110df5e6e6300598511cace0a442fde27f2
patrickryan <theyoungsoul@gmail.com>
2020-06-15 22:48:17 -0500

    intial

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# PyimageSearch OpenCV Social Distancing
+
+This repo contains my work going through the OpenCV Social Distancing blog post which can be found [here](https://www.pyimagesearch.com/2020/06/01/opencv-social-distancing-detector/)
+
+## Other resources
+
+https://www.learnopencv.com/deep-learning-based-object-detection-using-yolov3-with-opencv-python-c/
+
+https://www.learnopencv.com/training-yolov3-deep-learning-based-custom-object-detector/
+
+https://towardsdatascience.com/covid-19-ai-enabled-social-distancing-detector-using-opencv-ea2abd827d34
+
---
+++ social_distance_detector/pyimagesearch/detection.py
@@ -0,0 +1,89 @@
+# import the necessary packages
+import time
+
+from .social_distancing_config import NMS_THRESH
+from .social_distancing_config import MIN_CONF
+import numpy as np
+import cv2
+
+def detect_people(frame, net, ln, personIdx=0):
+	# grab the dimensions of the frame and  initialize the list of
+	# results
+	(H, W) = frame.shape[:2]
+	results = []
+
+	# construct a blob from the input frame and then perform a forward
+	# pass of the YOLO object detector, giving us our bounding boxes
+	# and associated probabilities
+	network_input_image_size = 320 # 416  320 allows for faster object detection
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (network_input_image_size, network_input_image_size),
+		swapRB=True, crop=False)
+	net.setInput(blob)
+	# starttime = time.time()
+	layerOutputs = net.forward(ln)
+	# stoptime = time.time()
+	# print("Video is Getting Processed at {:.4f} seconds per frame".format((stoptime - starttime)))
+
+	# initialize our lists of detected bounding boxes, centroids, and
+	# confidences, respectively
+	boxes = []
+	centroids = []
+	confidences = []
+
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		# loop over each of the detections
+		for detection in output:
+			# extract the class ID and confidence (i.e., probability)
+			# of the current object detection
+			# The first 4 elements represent the center_x, center_y, width and height.
+			# The fifth element represents the confidence that the bounding box encloses an object.
+			# The rest of the elements are the confidence associated with each class (i.e. object type).
+			# The box is assigned to the class corresponding to the highest score for the box.
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+
+			# filter detections by (1) ensuring that the object
+			# detected was a person and (2) that the minimum
+			# confidence is met
+			if classID == personIdx and confidence > MIN_CONF:
+				# scale the bounding box coordinates back relative to
+				# the size of the image, keeping in mind that YOLO
+				# actually returns the center (x, y)-coordinates of
+				# the bounding box followed by the boxes' width and
+				# height
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+
+				# use the center (x, y)-coordinates to derive the top
+				# and and left corner of the bounding box
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+
+				# update our list of bounding box coordinates,
+				# centroids, and confidences
+				boxes.append([x, y, int(width), int(height)])
+				centroids.append((centerX, centerY))
+				confidences.append(float(confidence))
+
+	# apply non-maxima suppression to suppress weak, overlapping
+	# bounding boxes
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
+
+	# ensure at least one detection exists
+	if len(idxs) > 0:
+		# loop over the indexes we are keeping
+		for i in idxs.flatten():
+			# extract the bounding box coordinates
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+
+			# update our results list to consist of the person
+			# prediction probability, bounding box coordinates,
+			# and the centroid
+			r = (confidences[i], (x, y, x + w, y + h), centroids[i])
+			results.append(r)
+
+	# return the list of results
+	return results
